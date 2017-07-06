97da1338799cb425d3340580dd312c5829fd0109
eleves <eleves@mac404inf-prof.local>
2017-07-06 16:21:35 -0400

    v2

--- JsonPHP/ViewController.swift
+++ JsonPHP/ViewController.swift
@@ -21,10 +21,34 @@ class ViewController: UIViewController {
     }
 
     @IBAction func save(_ sender: UIButton) {
+        let dictionary = ["a Key" : "aValue", "other Key" : "otherValue"]
+        var urlToSend = "http://localhost/dashboard/geneau/json_php/add.php?json=["
+        var counter = 0
+        let total = dictionary.count
+        for (a, b) in dictionary {
+            let noSpaces = replaceChars(originalStr: a, what: " ", byWhat: "_")
+            counter += 1
+            if counter < total {
+                urlToSend += "/\(noSpaces)/,/\(b)/!"
+            } else {
+                urlToSend += "/\(noSpaces)/,/\(b)/"
+            }
+        }
+        urlToSend += "]"
         
+        let session = URLSession.shared
+        let urlString = urlToSend
+        let url = NSURL(string: urlString)
+        let request = NSURLRequest(url: url! as URL)
+        let dataTask = session.dataTask(with: request as URLRequest) {
+            (data:Data?, response:URLResponse?, error:Error?) -> Void in
+        }
+        dataTask.resume()
     }
     
-
+    func replaceChars(originalStr: String, what: String, byWhat: String) -> String {
+        return originalStr.replacingOccurrences(of: what, with: byWhat)
+    }
 
 }
 
